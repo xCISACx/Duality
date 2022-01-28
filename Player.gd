886144e9e21b93ee66dd57b170028e2a2ba75226
CISAC <carolisacosta@hotmail.com>
2022-01-28 17:37:19 +0000

    character movement

---
+++ Player.gd
@@ -0,0 +1,58 @@
+extends KinematicBody2D
+
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var ROLL_SPEED = 120
+export var FRICTION = 500
+
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+var velocity = Vector2.ZERO;
+var roll_vector = Vector2.DOWN;
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if (input_vector != Vector2.ZERO):
+		roll_vector = input_vector
+		#swordHitbox.knockback_vector = input_vector
+		
+		#animationTree.set("parameters/Idle/blend_position", input_vector)
+		#animationTree.set("parameters/Run/blend_position", input_vector)
+		#animationTree.set("parameters/Attack/blend_position", input_vector)
+		#animationTree.set("parameters/Roll/blend_position", input_vector)
+		
+		#animationState.travel("Run")
+		
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		#animationState.travel("Idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	move()
+	
+func move():
+	velocity = move_and_slide(velocity)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
