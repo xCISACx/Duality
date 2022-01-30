ae40996dce548e0e50ce5c7fa432d3940c7b9525
CISAC <carolisacosta@hotmail.com>
2022-01-30 15:50:18 +0000

    added functional main menu with sound and fullscreen toggle

--- MainMenu.gd
+++ MainMenu.gd
@@ -12,7 +12,9 @@ onready var sfxAudioStream = $SettingsMenu/Menu/SettingsContainer/AudioSettings/
 onready var masterLabel = $SettingsMenu/Menu/SettingsContainer/AudioSettings/MasterValue
 onready var bgmLabel = $SettingsMenu/Menu/SettingsContainer/AudioSettings/BGMValue
 onready var sfxLabel = $SettingsMenu/Menu/SettingsContainer/AudioSettings/SFXValue
+onready var optionsButton = $SettingsMenu/Menu/SettingsContainer/VideoSettings/VideoOptionsButton
 var config = ConfigFile.new()
+var testing = false
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -82,8 +84,64 @@ func _on_SFXSlider_value_changed(value):
 
 
 func _on_BGMTestButton_pressed():
-	bgmAudioStream.play()
+	if testing == false:
+		bgmAudioStream.play()
+		testing = true
+	else:
+		bgmAudioStream.stop()
+		sfxAudioStream.stop()
+		testing = false
 
 
 func _on_SFXTestButton_pressed():
-	sfxAudioStream.play()
+	if testing == false:
+		sfxAudioStream.play()
+		testing = true
+	else:
+		bgmAudioStream.stop()
+		sfxAudioStream.stop()
+		testing = false
+
+
+func _on_CloseButton_pressed():
+	mainMenu.show()
+	settingsMenu.hide()
+	
+# Emitted when the selected resolution changes.
+signal resolution_changed(new_resolution)
+
+func _update_selected_item(text: String) -> void:
+	# The resolution options are written in the form "XRESxYRES".
+	# Using `split_floats` we get an array with both values as floats.
+	var values := text.split_floats("x")
+	# Emit a signal for informing the newly selected resolution
+	emit_signal("resolution_changed", Vector2(values[0], values[1]))
+
+func _on_VideoOptionsButton_item_selected(index):
+	_update_selected_item(optionsButton.text)
+	
+# Emitted when the user presses the "apply" button.
+signal apply_button_pressed(settings)
+
+# We store the selected settings in a dictionary
+var _settings := {resolution = Vector2(1280, 720), fullscreen = false, vsync = false}
+
+# Emit the `apply_button_pressed` signal, when user presses the button.
+func _on_ApplyButton_pressed() -> void:
+	# Send the last selected settings with the signal
+	print("apply")
+	emit_signal("apply_button_pressed", _settings)
+	update_settings(_settings)
+
+func _on_FullscreenCheckBox_toggled(button_pressed):
+	_settings.fullscreen = button_pressed
+
+func _on_StartMenu_resolution_changed(new_resolution):
+	_settings.resolution = new_resolution
+	
+func update_settings(settings: Dictionary) -> void:
+	OS.window_fullscreen = settings.fullscreen
+	#get_tree().set_screen_stretch(
+	#	SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_KEEP, settings.resolution
+	#)
+	#OS.set_window_size(settings.resolution)
