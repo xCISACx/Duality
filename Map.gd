c7914d961208fdacd310d837e767ed007995ef5b
inacioreina <74356253+inacioreina@users.noreply.github.com>
2022-01-30 15:25:22 +0000

    random generation

--- Map.gd
+++ Map.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
+var borders = Rect2(1, 1, 25, 25)
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const barrel = preload("res://EnemyBarrel.tscn")
+const mushroom = preload("res://EnemyMushroom.tscn")
+onready var tile_map = $TileMap
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	Variables.root = self
+<<<<<<< Updated upstream
 	BackgroundMusic.get_node("AudioStreamPlayer").play()
 	pass # Replace with function body.
 
@@ -16,3 +16,30 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+=======
+	generate_level()
+	
+func generate_level():
+	var walker = Walker.new(Vector2(19, 11), borders)
+	var map = walker.walk(1000)
+	walker.queue_free()
+	for location in map:
+		tile_map.set_cellv(location, -1)
+		if randf() <= 0.05:
+			generate_enemies(1, tile_map.map_to_world(location))
+			
+	tile_map.update_bitmask_region(borders.position, borders.end)
+		
+func generate_enemies(enemy_amount, location):
+		if randf() < .5:
+			var enemybarrel = barrel.instance()
+			add_child(enemybarrel)
+			enemybarrel.global_position = location
+		else:
+			var enemymushroom = mushroom.instance()
+			add_child(enemymushroom)
+			enemymushroom.global_position = location
+	
+func spawn_player():
+	pass
+>>>>>>> Stashed changes
